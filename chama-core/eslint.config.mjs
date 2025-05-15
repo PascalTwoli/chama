0341878e1e7357f62f276c2e751ca7734334691a
FelixTwoli <felixtwoli21@gmail.com>
2025-05-15 12:07:12 +0300

    chore: remove chama-core

--- chama-core/eslint.config.mjs
+++ chama-core/eslint.config.mjs
@@ -1,11 +1,58 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+const globals = require('globals');
+const typescript = require('@typescript-eslint/eslint-plugin');
+const prettier = require('eslint-plugin-prettier/recommended');
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-]);
+module.exports = [
+  {
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
+  },
+  prettier,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      import: require('eslint-plugin-import'),
+    },
+    rules: {
+      ...typescript.configs['recommended'].rules,
+      'import/order': [
+        'error',
+        {
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc' },
+        },
+      ],
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          trailingComma: 'all',
+          printWidth: 80,
+        },
+      ],
+    },
+  },
+];
---
+++ chama-core/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Chama Api')
+    .setDescription('The API details for the chama management system backend')
+    .setVersion('0.0.1')
+    .addTag('Chama Api documentation')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
