55e7bf9e8e9e44a70f1202baf1cb0eaead7d8656
FelixTwoli <felixtwoli21@gmail.com>
2025-05-28 00:55:16 +0300

    feat(user): enhance user management with update and registration improvements
    - Added update endpoint in UserController for modifying user details.
    - Updated UserService to handle user registration with enhanced response including authentication tokens and user details.
    - Improved error handling during user registration and deletion processes.
    - Refactored user data handling to ensure consistency between Firebase and local database.
    - Enhanced DTOs for user creation and registration to include optional fields and validation rules.

--- chama-core/src/user/user.controller.ts
+++ chama-core/src/user/user.controller.ts
@@ -11,14 +11,13 @@ import { UserService } from './user.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { Body } from '@nestjs/common';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   
-  // Auth endpoints have been moved to AuthController
-
   @Get()
   @UseGuards(AuthGuard)
   @ApiBearerAuth()
@@ -31,6 +30,13 @@ export class UserController {
   @ApiBearerAuth()
   findOne(@Param('id') id: string) {
     return this.userService.findOne(id);
+  }
+
+  @Patch(':id')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(id, updateUserDto);
   }
 
   @Delete(':id')
